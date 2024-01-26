9553b40e286883c2388103166a6658d264be8807
Iohan Soares <ioxsardinha@gmail.com>
2024-01-26 16:16:03 +0000

    Final, at least for MAMS

--- matsim-example-project/src/main/java/openoporto/analysis/CleanNetwork.java
+++ matsim-example-project/src/main/java/openoporto/analysis/CleanNetwork.java
@@ -1,5 +1,6 @@
 package openoporto.analysis;
 
+import com.graphhopper.reader.osm.OSMReader;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.config.ConfigUtils;
@@ -13,10 +14,11 @@ public class CleanNetwork {
     public static void main(String[] args) {
         String inputFile = "/home/iohan/Documentos/FEUP/MAMS/OpenOPorto/input/network.xml";
         String outputFile = "/home/iohan/Documentos/FEUP/MAMS/OpenOPorto/input/network_clean.xml";
-
         Network network = NetworkUtils.readNetwork(inputFile);
         NetworkCleaner networkCleaner = new NetworkCleaner();
         networkCleaner.run(network);
+
+        
 
         NetworkUtils.writeNetwork(network, outputFile);
     }
